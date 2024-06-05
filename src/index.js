a869658d3b0b048d40f309b1efef2124128c1c8d
wuwj <wuwj@allinpay.com>
2024-06-05 18:00:54 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose'); // 数据库
+const app = require('./app'); // 应用
+const config = require('./config/config'); // 配置
+const logger = require('./config/logger'); // 日志
+
+let server;
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  logger.info('Connected to MongoDB');
+  server = app.listen(config.port, () => {
+    logger.info(`Listening to port ${config.port}`);
+  });
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
